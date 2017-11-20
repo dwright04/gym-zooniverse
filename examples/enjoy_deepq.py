684ed3c1291d00c4f93b03afa3caeba19202c9a6
Darryl Wright <dwright04@qub.ac.uk>
2017-11-20 15:39:02 -0600

    Add enjoy_deepq.py for visualising trained deepq agents

---
+++ examples/enjoy_deepq.py
@@ -0,0 +1,28 @@
+import gym
+import argparse
+import gym_zooniverse
+
+from baselines import deepq
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--env', help='environment ID', default='SegmentationTestEnv-v0')
+    parser.add_argument('--model', help='saved deepq model', default='SegmentationTestEnv-v0.pkl')
+    
+    args = parser.parse_args()
+    
+    env = gym.make(args.env)
+    act = deepq.load(args.model)
+
+    while True:
+        obs, done = env.reset(), False
+        episode_rew = 0
+        while not done:
+            env.render()
+            obs, rew, done, _ = env.step(act(obs[None])[0])
+            episode_rew += rew
+        print("Episode reward", episode_rew)
+
+
+if __name__ == '__main__':
+    main()
